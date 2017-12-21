ebc52dad24e1be90fe353b2518795dfee086d326
mpiechocki <mpiechocki@arise.pl>
2017-12-21 20:19:41 +0100

    initial project commit

---
+++ SoundNotesInTime/SoundNotesInTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoundNotesInTime
+//
+//  Created by dontgonearthecastle on 21/12/2017.
+//  Copyright © 2017 dontgonearthecastle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoundNotesInTime/SoundNotesInTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundNotesInTime
+//
+//  Created by dontgonearthecastle on 21/12/2017.
+//  Copyright © 2017 dontgonearthecastle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
