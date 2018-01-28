3482f1d81e1f3a68c03d25781b60e000941aefcb
mpiechocki <mpiechocki@arise.pl>
2018-01-28 18:07:06 +0100

    Adding Metronome

--- SoundNotesInTime/SoundNotesInTime/ViewController.m
+++ SoundNotesInTime/SoundNotesInTime/ViewController.m
@@ -7,22 +7,25 @@
 //
 
 #import "ViewController.h"
+
+#import <AVFoundation/AVFoundation.h>
+
 #import <IGListKit/IGListKit.h>
 #import <Masonry/Masonry.h>
 
 #import "SectionTypeDescriptor.h"
-
 #import "TestSectionController.h"
 #import "PlaybackSectionController.h"
 #import "RecordingSectionController.h"
 #import "LibrarySectionController.h"
+#import "Metronome.h"
 
-@interface ViewController () <IGListAdapterDataSource>
+@interface ViewController () <IGListAdapterDataSource, MetronomeDelegate>
 
 @property (nonatomic, strong) UICollectionView *collectionView;
 @property (nonatomic, strong) IGListAdapter *adapter;
 @property NSArray<SectionTypeDescriptor *> *sections;
-
+@property Metronome *metronome;
 
 @end
 
@@ -50,6 +53,10 @@
 										   viewController:self];
 	self.adapter.collectionView = self.collectionView;
 	self.adapter.dataSource = self;
+	
+	[self setupAudioSession];
+	self.metronome = [[Metronome alloc] init];
+	self.metronome.delegate = self;
 }
 
 #pragma mark - IGListAdapterDataSource
@@ -71,6 +78,51 @@
 
 - (UIView *)emptyViewForListAdapter:(IGListAdapter *)listAdapter {
 	return nil;
+}
+
+- (void)setupAudioSession
+{
+	NSError *error = nil;
+	AVAudioSession *session = [AVAudioSession sharedInstance];
+	[session setCategory:AVAudioSessionCategoryAmbient error:&error];
+	if(error) {
+		NSLog(@"AVAudioSession error %ld, %@", error.code, error.localizedDescription);
+	}
+	[session setActive:true error:&error];
+	if(error) {
+		NSLog(@"AVAudioSession error %ld, %@", error.code, error.localizedDescription);
+	}
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(handleMediaServicesWereReset:)
+												 name:AVAudioSessionMediaServicesWereResetNotification
+											   object:session];
+}
+
+#pragma mark- AVAudioSession Notifications
+// see https://developer.apple.com/library/content/qa/qa1749/_index.html
+- (void)handleMediaServicesWereReset:(NSNotification *)notification
+{
+	NSLog(@"Media services have reset...");
+	
+	// tear down
+	self.metronome.delegate = nil;
+	self.metronome = nil;
+	
+	// re-create
+	self.metronome = [[Metronome alloc] init];
+	self.metronome.delegate = self;
+	
+	NSError *error = nil;
+	[[AVAudioSession sharedInstance] setActive:YES error:&error];
+	if(error) {
+		NSLog(@"AVAudioSession error %ld, %@", error.code, error.localizedDescription);
+	}
+}
+
+#pragma mark- Metronome Delegate
+- (void) tick
+{
+	NSLog(@"TICK");
 }
 
 @end
